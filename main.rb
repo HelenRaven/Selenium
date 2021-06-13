625ce4b1417676de0fc0cef7847edff431bedddf
HelenRaven <helenraven@mail.ru>
2021-06-13 10:22:21 +0700

    scrolling edited

--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ require 'selenium-webdriver'
 require 'csv'
 
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument("--disable-blink-features")
-options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument('--disable-blink-features')
+options.add_argument('--disable-blink-features=AutomationControlled')
 
 @driver = Selenium::WebDriver.for :chrome, options: options
 @driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
@@ -11,14 +11,10 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 @driver.manage.timeouts.implicit_wait = 10
 
 def slow_scroll(element)
-  scheight = 10
-  y = element.rect.y - element.rect.height
-
-  while scheight >= 0.7
-    @driver.execute_script("window.scrollTo(0,#{y/scheight})")
-    scheight -= 0.1
-  end
-  @driver.action.move_to(element).perform
+  win_height = @driver.manage.window.size.height
+  el_height = element.rect.height
+  @driver.execute_script("window.scrollTo(0,#{element.rect.y - win_height / 2 + el_height / 2}, { behavior: 'smooth'})")
+  sleep(1)
 end
 
 def load_pre_open_market_to_csv
@@ -29,21 +25,20 @@ def load_pre_open_market_to_csv
 
   @driver.find_element(:link_text, 'Pre-Open Market').click
 
-  rows = [['name', 'price']]
+  rows = [%w[name price]]
   sleep(1)
 
-  table = @driver.find_elements(:css, "table#livePreTable tbody tr")
+  table = @driver.find_elements(:css, 'table#livePreTable tbody tr')
 
   table.each do |tr|
     tds = tr.find_elements(:css, 'td')
     name = tds[1].find_element(:css, 'a').text
-    price = tds[6].text.tr(',','')
+    price = tds[6].text.tr(',', '')
     rows << [name, price]
   end
 
-  File.write("file.csv", rows.map(&:to_csv).join)
+  File.write('file.csv', rows.map(&:to_csv).join)
 end
-
 
 def user_scenary
   @driver.get 'https://www.nseindia.com/'
@@ -52,17 +47,17 @@ def user_scenary
   slow_scroll(nifty_bank)
   nifty_bank.click
 
-  view_all = @driver.find_element(:css, 'div#tab4_gainers_loosers img')
-  slow_scroll( view_all)
+  tb = @driver.find_element(:css, 'div#tab4_gainers_loosers')
+  slow_scroll(tb)
+  view_all = tb.find_element(:css, 'img')
   view_all.click
 
   select_element = @driver.find_element(id: 'equitieStockSelect')
   select_object = Selenium::WebDriver::Support::Select.new(select_element)
   select_object.select_by(:text, 'NIFTY ALPHA 50')
 
-  sleep(1)
+  sleep(5)
 end
-
 
 begin
   load_pre_open_market_to_csv
